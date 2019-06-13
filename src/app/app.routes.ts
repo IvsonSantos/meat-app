fe3c7ad8ba1735ce20f6a9a250b327cfe936dd2f
Ivson Santos <ivson.java@gmail.com>
2019-06-13 09:43:12 -0300

    Pagina de restaurantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 export const ROUTES: Routes = [
     { // quando nao digitar caminho, vai para o HOME
@@ -10,6 +11,10 @@ export const ROUTES: Routes = [
     { 
         path: 'about', 
         component: AboutComponent 
+    },
+    {
+        path: 'restaurants',
+        component: RestaurantsComponent
     }
 
 ]
