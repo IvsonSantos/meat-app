7d944b032f4cb149375f78003d536a959909b0a0
Ivson Santos <ivson.java@gmail.com>
2019-06-13 17:36:20 -0300

    Adicionando o service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './about/about.component'
 import { ROUTES } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantService } from './restaurants/restaurant/restaurant.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,9 @@ import { RestaurantComponent } from './restaurants/restaurant/restaurant.compone
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [
+    RestaurantService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
