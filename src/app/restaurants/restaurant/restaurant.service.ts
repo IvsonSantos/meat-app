e965dacd0a012dd9116e2122e1800ebb2d038013
Ivson Santos <ivson.java@gmail.com>
2019-06-14 17:03:09 -0300

    Error Handler

--- src/app/restaurants/restaurant/restaurant.service.ts
+++ src/app/restaurants/restaurant/restaurant.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from "@angular/core";
 import { Restaurant } from "./restaurant.model";
 import { Http } from "@angular/http";
 import { MET_API } from "app/app.api";
-import { Observable } from "rxjs/observable"
-import "rxjs/add/operator/map"
-import "rxjs/add/operator/catch"
+import { Observable } from "rxjs/observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import { ErrorHandler } from "app/app.error-handler";
 
 // vamos receber um outro servico
 @Injectable()
@@ -17,10 +18,10 @@ export class RestaurantService {
     }
 
     restaurants(): Observable<Restaurant[]> {
-
         // usamos o MAP para pegar apenas o objeto JSON e nao toda o RESPONSE
         return this.http.get(`${MET_API}/restaurants`)
-                        .map(response => response.json())    
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
 
 
